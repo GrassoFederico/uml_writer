6c76e2b7a5a1080ea300a1d7e3072e32c312457d
Federico Grasso <gras.federico@gmail.com>
2020-07-31 22:43:42 +0200

    Commentato frasi di struttura del programma

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-print("Input:\t percorso cartella + percorso file di output")
+#Input: percorso cartella + percorso file di output
 
 #!/usr/bin/env python3
 
 from uw.system import *
 
 def main():
-    print(add('1', '1'))
+    print(get_directory_path())
 
 if __name__ == '__main__':
     main()
 
-print("Process: converte l'alberatura in markup UML")
-print("Process: salva il risultato nel percorso di output")
+#Process: converte l'alberatura in markup UML
+#Process: salva il risultato nel percorso di output
