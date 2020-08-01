d4326e986edeebedef4f54b4ce386546570f2a85
Federico Grasso <gras.federico@gmail.com>
2020-08-01 19:03:49 +0200

    Controllo se i valori passati sono corretti
    - Se sono corretti continuo ad eseguire il programma
    - Se i dati non sono corretti ritorno al sistema operativo -1

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from uw.system import get_parameters
 
 def main():
     directory_path, output_path = get_parameters()
+    
+    if(directory_path and output_path):
+        print("Processo")
+        return 0
+    else:
+        return -1
 
 if __name__ == '__main__':
     main()
