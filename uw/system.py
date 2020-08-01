46fee9b8107c33b51014bc1d8f89f48a32d5f4d0
Federico Grasso <gras.federico@gmail.com>
2020-08-01 18:15:43 +0200

    Fix tunzione get_directory_path()
    Restituisce una tupla

--- uw/system.py
+++ uw/system.py
@@ -2,7 +2,7 @@
 import sys
 
 # Exported functions
-def get_directory_path():
+def get_directory_path() -> tuple:
     try:
         return sys.argv[1], sys.argv[2] #argv[1] -> directory path, argv[2] -> output_path
     except IndexError:
@@ -11,7 +11,7 @@ def get_directory_path():
 
 # Test functions for module  
 def _test():
-    assert isinstance( get_directory_path(), str )
+    assert isinstance(get_directory_path(), tuple)
 
 if __name__ == '__main__':
     _test()
