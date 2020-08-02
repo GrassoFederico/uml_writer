f49f47015a0be34555abf8eb4dec0791e28aff7b
Federico Grasso <gras.federico@gmail.com>
2020-08-02 20:06:53 +0200

    Implementato test istanza di Code

--- uw/converter.py
+++ uw/converter.py
@@ -9,17 +9,25 @@ class UML_markdown:
 
 class Code:
 
-    pass
+    def __init__(self):
+        self._file_content = "test"
 
 # Test functions for module  
 def _test():
     _test_UML_markdown_class()
+    _test_Code_class()
 
 def _test_UML_markdown_class():
     uml_markdown = UML_markdown()
     
     assert isinstance(uml_markdown, UML_markdown)
     assert isinstance(uml_markdown._code, Code)
+
+def _test_Code_class():
+    code = Code()
+
+    assert isinstance(code, Code)
+    assert isinstance(code._file_content, str)
 
 if __name__ == '__main__':
     _test()
