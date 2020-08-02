f5330df69acf20e199e706edc58f7939eb071087
Federico Grasso <gras.federico@gmail.com>
2020-08-02 19:53:51 +0200

    Init classe Code

--- uw/converter.py
+++ uw/converter.py
@@ -2,7 +2,13 @@
 from system import open_file
 
 # Exported functions
-class UML_markdown():
+class UML_markdown:
+    
+    def __init__(self):
+        self._code = Code
+
+class Code:
+
     pass
 
 # Test functions for module  
@@ -13,6 +19,7 @@ def _test_UML_markdown_class():
     uml_markdown = UML_markdown()
 
     assert type(uml_markdown) is UML_markdown
+    assert uml_markdown._code is Code
 
 if __name__ == '__main__':
     _test()
