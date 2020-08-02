b27fcc978e0e157dceeaa7446ef12c6845981657
Federico Grasso <gras.federico@gmail.com>
2020-08-02 19:47:13 +0200

    Init classe UML_markdown()

--- uw/converter.py
+++ uw/converter.py
@@ -2,10 +2,17 @@
 from system import open_file
 
 # Exported functions
-
+class UML_markdown():
+    pass
 
 # Test functions for module  
 def _test():
+    _test_UML_markdown_class()
+
+def _test_UML_markdown_class():
+    uml_markdown = UML_markdown()
+
+    assert type(uml_markdown) is UML_markdown
 
 if __name__ == '__main__':
     _test()
