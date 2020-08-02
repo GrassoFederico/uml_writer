1a4ac5ae8f72ce1170e698cc98f67f6ce5b1d22c
Federico Grasso <gras.federico@gmail.com>
2020-08-02 12:20:01 +0200

    Testato le funzionalità del modulo "os"

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from uw.system import get_parameters
+from uw.system import get_parameters, explore_directory_path
 
 def main():
     directory_path, output_path = get_parameters()
     
     if(directory_path and output_path):
-        print("Processo")
+        explore_directory_path(directory_path)
         return 0
     else:
         return -1
--- uw/system.py
+++ uw/system.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
 
 # Exported functions
 def get_parameters() -> tuple:
     try:
-        return sys.argv[1], sys.argv[2] #argv[1] -> directory path, argv[2] -> output_path
+        return sys.argv[1], sys.argv[2] # argv[1] -> directory path, argv[2] -> output_path
     except IndexError:
         print("USAGE: uw.exe [directory_path] [output_path]")
         return False, False
+
+def explore_directory_path(directory_path):
+    for root, directoy_names, file_names in os.walk(directory_path):
+        for file_name in file_names:
+            print(os.path.join(root, file_name))
 
 # Test functions for module  
 def _test():
