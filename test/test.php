14ed216dce3ccfb02e78170ba46e1039f545e25a
Federico Grasso <gras.federico@gmail.com>
2020-08-07 10:47:22 +0200

    Caricato file di test più complesso
    Per testare i limiti delle regex

--- test/test.php
+++ test/test.php
@@ -1,27 +1,122 @@
 <?php
 
-namespace App;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Repositories\Payments\PayPal\Response;
+use App\Repositories\Payments\RemoteController;
+use App\Repositories\Payments\Device;
+use App\Repositories\Facade\Invoice;
+use App\Repositories\Facade\Offer;
+use App\Events\OrderCompleted;
+use App\PaymentType;
+use App\PaymentTransition;
+use App\Events\AddressUpdated;
 
-class Accelerator extends Model
+class PaymentController extends Controller
 {
-    protected $fillable = ['payment_type_id', 'product_id', 'offer_id'];
+    private $order;
+    private $remote_controller;
+    private $payment_identifier;
+    private $payer_identifier;
 
-    protected $hidden = ['payment_type_id', 'product_id', 'offer_id'];
-
-    public function payment_type()
+    public function create(Request $request)
     {
-        return $this->belongsTo('\App\PaymentType', 'payment_type_id');
+        $this->init_components($request);
+
+        if($request->error)
+            return redirect()->back()->withErrors(['pay' => $request->error]);
+        else
+            return redirect()->away($this->remote_controller->get_payment_link());
     }
 
-    public function product()
+    public function return(Request $request)
     {
-        return $this->belongsTo('\App\Product');
+        $this->init_components($request);
+
+        if(empty($this->payment_identifier))
+            return back();
+
+        $registry = $this->remote_controller->get_payer_info( $this->payment_identifier );
+        return view('pages.pay', ['registry' => $registry]);
     }
 
-    public function offer()
+    public function execute(Request $request)
     {
-        return $this->belongsTo('\App\Offer', 'offer_id');
+        $this->init_components($request);
+
+        event(new AddressUpdated($this->order, $request));
+
+        if(empty($this->payment_identifier) || empty($this->payer_identifier))
+            return back();
+
+        $result = $this->remote_controller->get_payment_result( $this->payment_identifier, $this->payer_identifier );
+        $state = strtoupper($result->state);
+
+        if(($state === config('order.status.completed')) || ($state === config('order.status.pending')))
+        {
+            event(new OrderCompleted($this->order));
+            return redirect()->route('thankyou');
+        }
+        elseif( $result->state === Response::NO_FUNDS )
+            return redirect()->away($this->remote_controller->get_payment_link($this->payment_identifier));
+        else
+            return redirect()->route('checkout')->withErrors(['pay' => $result->error]);
+    }
+
+    public function cancel(Request $request)
+    {
+        return redirect()->route('checkout')->withErrors([
+            'pay' => __('payments.cancel')
+        ]);
+    }
+
+    private function init_components(Request $request): void
+    {
+        $request->validate([
+            'payment' => ['required', 'string', Rule::in(config('payments.available'))]
+        ]);
+
+        $this->order = \App\Order::find( session('order_id') );
+        
+        $this->init_payment_transition( $this->get_payment_type($request->payment) );
+        $this->offer = Offer::build($this->order);
+        $this->invoice = Invoice::build($this->order);
+
+        $this->remote_controller = new RemoteController( $this->get_payment_device( $request ) );
+    }
+
+    private function init_payment_transition(PaymentType $payment_type): void
+    {
+        PaymentTransition::updateOrCreate([
+                'order_id' => $this->order->id
+            ], [
+                'payment_type_id' => $payment_type->description,
+                'amount' => $this->order->order_total
+            ]);
+    }
+
+    private function get_payment_type(string $payment_type_description): PaymentType
+    {
+        return PaymentType::find($payment_type_description) ?? PaymentType::find( config('payments.default') );
+    }
+
+    private function get_payment_device(Request $request)
+    {
+        if($request->payment === 'PAYPAL')
+        {
+            $this->payer_identifier = $request->PayerID;
+            $this->payment_identifier = $request->paymentId;
+
+            return new \App\Repositories\Payments\PayPalDevice( $this->invoice );
+        }
+        else
+        {
+            $this->payer_identifier = $this->order->id;
+            $this->payment_identifier = $this->order->id;
+
+            return new \App\Repositories\Payments\CashOnDeliveryDevice;
+        }
     }
 }
