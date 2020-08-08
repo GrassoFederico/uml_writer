7bf7f9808854f3ed191375865ba10bad3b9f3cd4
Federico Grasso <gras.federico@gmail.com>
2020-08-08 14:03:08 +0200

    Implementato split code by entity
    In questo modo sono in grado di associare ciascun frammento di codice alla classe originaria

--- uw/converter.py
+++ uw/converter.py
@@ -20,7 +20,7 @@ class UML_markdown(ABC):
             raise Exception(_FILE_FORMAT_NOT_SUPPORTED)
 
     @abstractmethod
-    def build_classes(self):
+    def build_entities(self):
         pass
 
     @abstractmethod
@@ -33,8 +33,8 @@ class UML_markdown(ABC):
 
 class PlantUML(UML_markdown):
 
-    def build_classes(self):
-        return self._code.get_classes()
+    def build_entities(self):
+        return self._code.get_entities()
 
     def build_properties(self):
         return self._code.get_properties()
@@ -52,6 +52,10 @@ class Code(ABC):
             print( open_file(file_path, 'r') )
     
     @abstractmethod
+    def split_by_entity(self) -> list:
+        pass
+    
+    @abstractmethod
     def get_namespace(self) -> list:
         pass
 
@@ -60,7 +64,7 @@ class Code(ABC):
         pass
 
     @abstractmethod
-    def get_classes(self) -> list:
+    def get_entities(self) -> list:
         pass
 
     @abstractmethod
@@ -73,11 +77,16 @@ class Code(ABC):
 
 class PHP(Code):
 
+    __split_by_entities_regex = r'.*class.*|.*interface.*'
+
     __namespace_regex = r' *namespace +([\\\w]+)'
     __dependencies_regex = r' *use +([\\\w]+)'
-    __classes_regex = r' *(\w+)? *(class|interface) +(\w+) *(extends|implements)? *(\w+)?'
+    __entities_regex = r' *(\w+)? *(class|interface) +(\w+) *(extends|implements)? *(\w+)?'
     __properties_regex = r'[ \t]+([abstract|static| ]+)? *(protected|public|private)? +\$(?![\w]+\->)(\w+)'
     __methods_regex = r' *([abstract|static| ]+)? *(protected|public|private)? *function +(\w+)\(([\\\w \$]*)\)[: ]*([\\\w]*)'
+
+    def split_by_entity(self) -> list:
+        return re.split(self.__split_by_entities_regex, self._file_content)
 
     def get_namespace(self) -> list:
         return re.findall(self.__namespace_regex, self._file_content)
@@ -85,8 +94,8 @@ class PHP(Code):
     def get_dependencies(self) -> list:
         return re.findall(self.__dependencies_regex, self._file_content)
 
-    def get_classes(self) -> list:
-        return re.findall(self.__classes_regex, self._file_content)
+    def get_entities(self) -> list:
+        return re.findall(self.__entities_regex, self._file_content)
 
     def get_properties(self) -> list:
         return re.findall(self.__properties_regex, self._file_content)
@@ -96,13 +105,16 @@ class PHP(Code):
 
 class Vue(Code):
 
+    def split_by_entity(self) -> list:
+        pass
+
     def get_namespace(self) -> list:
         pass
 
     def get_dependencies(self) -> list:
         pass
 
-    def get_classes(self) -> list:
+    def get_entities(self) -> list:
         return ["test"]
 
     def get_properties(self) -> list:
@@ -115,6 +127,7 @@ class Vue(Code):
 def _test():
     _test_UML_markdown_class()
     _test_Code_extended_classes_file_content()
+    _test_PHP_split_data()
     _test_PHP_extract_data()
 
 def _test_UML_markdown_class():
@@ -133,19 +146,27 @@ def _test_Code_extended_classes_file_content():
     assert isinstance(php_uml_markdown._code._file_content, str)
     assert isinstance(vue_uml_markdown._code._file_content, str)
 
-def _test_PHP_extract_data():
-    php_test_file_namespace = ['App\\Http\\Controllers']
-    php_test_file_dependencies = ['Illuminate\\Http\\Request', 'Illuminate\\Validation\\Rule', 'App\\Repositories\\Payments\\PayPal\\Response', 'App\\Repositories\\Payments\\RemoteController', 'App\\Repositories\\Payments\\Device', 'App\\Repositories\\Facade\\Invoice', 'App\\Repositories\\Facade\\Offer', 'App\\Events\\OrderCompleted', 'App\\PaymentType', 'App\\PaymentTransition', 'App\\Events\\AddressUpdated']
-    php_test_file_classes = [('abstract', 'class', 'Test', '', ''), ('', 'class', 'PaymentController', 'extends', 'Controller')]
-    php_test_file_properties = [('', 'private', 'order'), ('', 'private', 'remote_controller'), ('', 'private', 'payment_identifier'), ('', 'private', 'order'), ('', 'private', 'remote_controller'), ('', 'private', 'payment_identifier'), ('', 'private', 'payer_identifier'), ('', '', 'registry'), ('', '', 'result'), ('', '', 'state')]
-    php_test_file_methods = [('', 'public', 'create', 'Request $request', ''), ('', 'public', 'return', 'Request $request', ''), ('', 'public', 'execute', 'Request $request', ''), ('', 'public', 'cancel', 'Request $request', ''), ('', 'private', 'init_components', 'Request $request', 'void'), ('', 'private', 'init_payment_transition', 'PaymentType $payment_type', 'void'), ('', 'private', 'get_payment_type', 'string $payment_type_description', 'PaymentType'), ('', 'private', 'get_payment_device', 'Request $request', '')]
+def _test_PHP_split_data():
+    test_file_split_number = 3
+
     php_code = PHP('./test/test.php')
 
-    assert php_code.get_namespace() == php_test_file_namespace
-    assert php_code.get_dependencies() == php_test_file_dependencies
-    assert php_code.get_classes() == php_test_file_classes
-    assert php_code.get_properties() == php_test_file_properties
-    assert php_code.get_methods() == php_test_file_methods
+    assert len(php_code.split_by_entity()) == test_file_split_number
+
+def _test_PHP_extract_data():
+    test_file_namespace = ['App\\Http\\Controllers']
+    test_file_dependencies = ['Illuminate\\Http\\Request', 'Illuminate\\Validation\\Rule', 'App\\Repositories\\Payments\\PayPal\\Response', 'App\\Repositories\\Payments\\RemoteController', 'App\\Repositories\\Payments\\Device', 'App\\Repositories\\Facade\\Invoice', 'App\\Repositories\\Facade\\Offer', 'App\\Events\\OrderCompleted', 'App\\PaymentType', 'App\\PaymentTransition', 'App\\Events\\AddressUpdated']
+    test_file_classes = [('abstract', 'class', 'Test', '', ''), ('', 'class', 'PaymentController', 'extends', 'Controller')]
+    test_file_properties = [('', 'private', 'order'), ('', 'private', 'remote_controller'), ('', 'private', 'payment_identifier'), ('', 'private', 'order'), ('', 'private', 'remote_controller'), ('', 'private', 'payment_identifier'), ('', 'private', 'payer_identifier'), ('', '', 'registry'), ('', '', 'result'), ('', '', 'state')]
+    test_file_methods = [('', 'public', 'create', 'Request $request', ''), ('', 'public', 'return', 'Request $request', ''), ('', 'public', 'execute', 'Request $request', ''), ('', 'public', 'cancel', 'Request $request', ''), ('', 'private', 'init_components', 'Request $request', 'void'), ('', 'private', 'init_payment_transition', 'PaymentType $payment_type', 'void'), ('', 'private', 'get_payment_type', 'string $payment_type_description', 'PaymentType'), ('', 'private', 'get_payment_device', 'Request $request', '')]
+    
+    code = PHP('./test/test.php')
+
+    assert code.get_namespace() == test_file_namespace
+    assert code.get_dependencies() == test_file_dependencies
+    assert code.get_entities() == test_file_classes
+    assert code.get_properties() == test_file_properties
+    assert code.get_methods() == test_file_methods
 
 if __name__ == '__main__':
     _test()
