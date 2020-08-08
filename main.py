19da223eccd5ae4d13b60f4aea20d0fe76a9ebc7
Federico Grasso <gras.federico@gmail.com>
2020-08-08 15:33:57 +0200

    Implementato su Main il primo processo di generazione UML

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+import io
+import sys
+import plantuml
+import uw.converter
+from PIL import Image
 from uw.system import get_parameters, get_directory_file_names
 
 def main():
@@ -8,7 +13,24 @@ def main():
     if(directory_path and output_path):
         print('Estraggo i file...')
         file_names = get_directory_file_names(directory_path)
-        #print("\n".join(file_names))
+
+        markdown = ''
+
+        for file_name in file_names:
+            try:
+                plant_uml_converter = uw.converter.PlantUML(file_name)
+                markdown += plant_uml_converter.build_entities()
+            except Exception as error:
+                print(error)
+
+        plant_uml_processor = plantuml.PlantUML(url='http://www.plantuml.com/plantuml/img/')
+        raw_data = plant_uml_processor.processes( markdown )
+
+        stream = io.BytesIO(raw_data)
+
+        img = Image.open(stream)
+        img.save('foo.png')
+
         return 0
     else:
         return -1
